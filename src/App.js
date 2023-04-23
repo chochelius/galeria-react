483ad438b1570d31eddd11a9a8218ab89500093f
Jorge Cortes <chochelius@gmail.com>
2023-04-23 15:04:30 -0400

    third commit

--- src/App.js
+++ src/App.js
@@ -14,16 +14,16 @@ function App() {
         <div className="row">
           
           <div className="col-md-6 ml-auto px-1 py-1">
-            <Card image="https://picsum.photos/seed/lorem/200/200" title="Card 1" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae elit libero, a pharetra augue. Donec id elit non mi porta gravida at eget metus." buttontext="Ver Mas" />
+            <Card image="https://picsum.photos/seed/lorem/200/200" title="Tarjeta 1" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae elit libero, a pharetra augue. Donec id elit non mi porta gravida at eget metus." buttontext="Ver Mas" />
           </div>
           <div className="col-md-6 ml-auto px-1 py-1">
-            <Card image="https://picsum.photos/seed/picsum/200/200" title="Card 2" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae elit libero, a pharetra augue. Donec id elit non mi porta gravida at eget metus." buttontext="Ver Mas" />
+            <Card image="https://picsum.photos/seed/picsum/200/200" title="Tarjeta 2" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae elit libero, a pharetra augue. Donec id elit non mi porta gravida at eget metus." buttontext="Ver Mas" />
           </div>
           <div className="col-md-6 ml-auto px-1 py-1">
-            <Card image="https://picsum.photos/seed/dolor/200/200" title="Card 3" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae elit libero, a pharetra augue. Donec id elit non mi porta gravida at eget metus." buttontext="Ver Mas" />
+            <Card image="https://picsum.photos/seed/dolor/200/200" title="Tarjeta 3" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae elit libero, a pharetra augue. Donec id elit non mi porta gravida at eget metus." buttontext="Ver Mas" />
           </div>
           <div className="col-md-6 ml-auto px-1 py-1">
-            <Card image="https://picsum.photos/seed/sit/200/200" title="Card 3" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae elit libero, a pharetra augue. Donec id elit non mi porta gravida at eget metus." buttontext="Ver Mas" />
+            <Card image="https://picsum.photos/seed/sit/200/200" title="Tarjeta 4" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae elit libero, a pharetra augue. Donec id elit non mi porta gravida at eget metus." buttontext="Ver Mas" />
           </div>
           <Footer />
         </div>
